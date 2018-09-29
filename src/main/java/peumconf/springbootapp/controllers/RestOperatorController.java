8d2ece0466134de3c5a885b6d637fa9b29ddfe9a
aleamb <jandroz@gmail.com>
2018-09-29 09:24:19 +0200

    Added Operators custom REST service

---
+++ src/main/java/peumconf/springbootapp/controllers/RestOperatorController.java
@@ -0,0 +1,22 @@
+package peumconf.springbootapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import peumconf.springbootapp.model.Operator;
+import peumconf.springbootapp.repository.OperatorRepository;
+
+@RestController
+public class RestOperatorController {
+
+	@Autowired
+	private OperatorRepository operatorRepository;
+
+	@GetMapping("/operators")
+	public Iterable<Operator> getAll() {
+
+		return operatorRepository.findAll();
+
+	}
+}
